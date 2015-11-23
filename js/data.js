e332792225c3a3dcdcfb27a73a0bd7568cf9e452
ptwing <xiao@ptwing.com>
2015-11-23 14:24:10 +0800

    更新首页图片

--- js/data.js
+++ js/data.js
@@ -2,12 +2,9 @@ var nIndex 	= 1;
 var oIndex 	= 0;
 var delay 	= 3000;
 var jsonSlidPic = [
-                    {"small_pic_src":"images/slid01.jpg", "big_pic_src":"images/slid01.jpg", "game_url":"#"}, 
-                   	{"small_pic_src":"images/slid02.jpg", "big_pic_src":"images/slid02.jpg", "game_url":"#"}, 
-                   	{"small_pic_src":"images/slid03.jpg", "big_pic_src":"images/slid03.jpg", "game_url":"#"}, 
-                    {"small_pic_src":"images/slid04.jpg", "big_pic_src":"images/slid04.jpg", "game_url":"#"}, 
-                    {"small_pic_src":"images/slid05.jpg", "big_pic_src":"images/slid05.jpg", "game_url":"#"}, 
-                    {"small_pic_src":"images/slid06.jpg", "big_pic_src":"images/slid06.jpg", "game_url":"#"}
+                    {"small_pic_src":"images/index01.jpg", "big_pic_src":"images/index01.jpg", "game_url":"#"},
+                   	{"small_pic_src":"images/index02.jpg", "big_pic_src":"images/index02.jpg", "game_url":"#"},
+                    {"small_pic_src":"images/index03.jpg", "big_pic_src":"images/index03.jpg", "game_url":"#"}
 	              ];
 var jsonNews = [
 	              {"news_date":"", "news_href":"#", "news_desc":""}
@@ -18,8 +15,8 @@ var jsonFollow = [
 var jsonGames = [
                   {"game_url":"#", "img_src":"images/slid01.jpg", "game_desc":"多種平臺登陸"},
                   {"game_url":"#", "img_src":"images/slid02.jpg", "game_desc":"多樣遊戲模式，盡情暢玩"},
-	              {"game_url":"#", "img_src":"images/slid03.jpg", "game_desc":"靚麗UI設計"},
-	              {"game_url":"#", "img_src":"images/slid04.jpg", "game_desc":"好友互動"},
-	              {"game_url":"#", "img_src":"images/slid05.jpg", "game_desc":"24小時使用者回饋服務"},
-	              {"game_url":"#", "img_src":"images/slid06.jpg", "game_desc":"專人負責遊戲主頁群組"}
+	                {"game_url":"#", "img_src":"images/slid03.jpg", "game_desc":"靚麗UI設計"},
+	                {"game_url":"#", "img_src":"images/slid04.jpg", "game_desc":"好友互動"},
+	                {"game_url":"#", "img_src":"images/slid05.jpg", "game_desc":"24小時使用者回饋服務"},
+	                {"game_url":"#", "img_src":"images/slid06.jpg", "game_desc":"專人負責遊戲主頁群組"}
                 ];
