e78f130381802d84431ea55b10197d374a4c83cd
ptwing <xiao@ptwing.com>
2015-12-28 17:39:06 +0800

    update

--- js/data.js
+++ js/data.js
@@ -13,10 +13,10 @@ var jsonFollow = [
                   {"pic_src":"", "follow_url":"#"}
 	              ];
 var jsonGames = [
-                  {"game_url":"#", "img_src":"images/slid01.jpg", "game_desc":"多種平臺登陸"},
-                  {"game_url":"#", "img_src":"images/slid02.jpg", "game_desc":"多樣遊戲模式，盡情暢玩"},
-	                {"game_url":"#", "img_src":"images/slid03.jpg", "game_desc":"靚麗UI設計"},
-	                {"game_url":"#", "img_src":"images/slid04.jpg", "game_desc":"好友互動"},
-	                {"game_url":"#", "img_src":"images/slid05.jpg", "game_desc":"24小時使用者回饋服務"},
-	                {"game_url":"#", "img_src":"images/slid06.jpg", "game_desc":"專人負責遊戲主頁群組"}
+                  {"game_url":"#", "img_src":"images/slid01.jpg", "game_desc":"多种平台登录"},
+                  {"game_url":"#", "img_src":"images/slid02.jpg", "game_desc":"多种游戏模式，尽情畅玩"},
+	                {"game_url":"#", "img_src":"images/slid03.jpg", "game_desc":"靓丽的UI设计"},
+	                {"game_url":"#", "img_src":"images/slid04.jpg", "game_desc":"好友互动"},
+	                {"game_url":"#", "img_src":"images/slid05.jpg", "game_desc":"24小时使用者反馈服务"},
+	                {"game_url":"#", "img_src":"images/slid06.jpg", "game_desc":"专人负责游戏主页群组"}
                 ];
